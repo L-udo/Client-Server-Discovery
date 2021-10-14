5eef924555395b2283f17decac31afc4008fb07c
LudoW809 <almondjunior@gmail.com>
2021-10-14 17:55:30 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,12 +1,13 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port = 25565
-sock.bind(('', port))
-found = '', ''
+def search():
+    import socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    port = 25565
+    sock.bind(('', port))
+    found = '', ''
 
 
-while found[0] != 'LD_FIND':
-    msg, addr = sock.recvfrom(1024)
-    found = msg.decode('utf-8').split(':')
-    server_ip = addr[0]
-    print(found[1])
+    while found[0] != 'LD_FIND':
+        msg, addr = sock.recvfrom(1024)
+        found = msg.decode('utf-8').split(':')
+        print(found[1])
+    return addr[0]
